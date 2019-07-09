c924227abce48ca36fc4a7c597c33810e7020b76
Tran Anh Dung <trandunguet@gmail.com>
2019-07-10 02:11:05 +0700

    register ok

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/online-quiz', { useNewUrlParser: true, useCreateIndex: true }, (err) => {
+    if (err) {
+        console.log(err);
+    }
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -25,19 +32,19 @@ app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,12 +1,21 @@
+// var session = require('express-session');
+var mongoose = require('mongoose');
 var express = require('express');
-var router = express.Router();
+var userSchema = require('../../models/user.model');
 
-router.post('/login', function(req, res, next) {
-  res.send(req.body.name + ',,, ' + req.body.password);
+var router = express.Router();
+var User = mongoose.model('User', userSchema);
+
+router.post('/login', function (req, res, next) {
+    res.send(req.body.name + ',,, ' + req.body.password);
 });
 
-router.post('/signup', function(req, res, next) {
-    res.send(req.body.name + ',,, ' + req.body.password);
+router.post('/signup', function (req, res, next) {
+    User.create({username: req.body.name, password: req.body.password}, function(err, _)
+    {
+        console.log(err);
+    });
+    res.redirect('../../signup-login');
 });
 
 module.exports = router;
