703f42e7e071c1aa9f3557dd08c8f9d27548428f
Tran Anh Dung <trandunguet@gmail.com>
2019-07-22 16:22:01 +0700

    fix typo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    var text = 'Hello, guess';
+    var text = 'Hello, guest';
     if (req.session.user) {
         text = 'Hello, ' + req.session.user;
     }
